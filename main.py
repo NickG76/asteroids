f125ab6f86236499d4d66c4e09a583b0f079b4fb
NickG76 <nickgirling76@gmail.com>
2025-06-17 17:14:42 +0100

    added time.click() and tick()

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt += pygame.time.Clock().tick(60) / 1000
+        print(f"Delta Time: {dt:.2f} seconds")
     
 
 if __name__ == "__main__":
