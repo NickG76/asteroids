6c7e8410857958921456771369645d4fe4d55dc4
NickG76 <nickgirling76@gmail.com>
2025-06-18 10:45:16 +0100

    Fixed rotation on the a and d keys, due to the layout of the while statement, it would revert back to 0

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
-        screen.fill("black")
-        dt += pygame.time.Clock().tick(60) / 1000
-        player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-
 
         player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-    
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
